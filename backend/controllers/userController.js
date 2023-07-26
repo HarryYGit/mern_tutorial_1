b49aaf43205797b2eb6606a2a5e5c20eb10a8e76
Haoran Yan <yharrytsy@gmail.com>
2023-07-26 21:23:22 +1000

    DONE AUTH APIS

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -42,7 +42,8 @@ const registerUser = asyncHandler( async (req, res) => {
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
 
 
         })
@@ -67,7 +68,8 @@ const loginUser = asyncHandler( async (req, res) => {
         res.json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id),
         })
     } else {
         res.status(400)
@@ -81,10 +83,26 @@ const loginUser = asyncHandler( async (req, res) => {
 
 // @desc Get user data
 // @route GET /api/users/ME
-//  @access Public
+//  @access Private
 const getMe = asyncHandler( async (req, res) => {
-    res.json({ message: 'User data display'})
+    //res.json({ message: 'User data display'})
+    const {_id, name, email} = await User.findById(req.user.id)
+
+    res.status(200).json({
+        id: _id,
+        name,
+        email,
+    })
 })
+
+
+//generate JWT web token
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    })
+}
+
 
 
 module.exports = {
