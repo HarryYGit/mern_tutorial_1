607f439dbf5f6b2636488bd102c0c0785fef7661
Haoran Yan <yharrytsy@gmail.com>
2023-07-27 19:27:26 +1000

    register backend with frontend and test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Header from './components/header'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
@@ -21,6 +23,7 @@ function App() {
         </div>
 
       </Router>
+      <ToastContainer />
     </>
     );
 }
