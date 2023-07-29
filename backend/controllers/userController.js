c403817d3f4aa28f76e5349c468da1c36dcdedb2
Haoran Yan <yharrytsy@gmail.com>
2023-07-29 23:20:19 +1000

    update frontend functions

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -85,14 +85,9 @@ const loginUser = asyncHandler( async (req, res) => {
 // @route GET /api/users/ME
 //  @access Private
 const getMe = asyncHandler( async (req, res) => {
-    //res.json({ message: 'User data display'})
-    const {_id, name, email} = await User.findById(req.user.id)
 
-    res.status(200).json({
-        id: _id,
-        name,
-        email,
-    })
+
+    res.status(200).json(req.user)
 })
 
 
